4f623bb0cbc7ac3c04a9defd0de35aa8b2a04608
AJKaufman <ak3012@rit.edu>
2017-03-08 05:14:12 -0500

    Added an add quote with name of speaker feature

---
+++ src/responses.js
@@ -0,0 +1,156 @@
+// Code taken from Aidan Kaufman Http-Api-Assignment-2
+const fs = require('fs');
+const crypto = require('crypto');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+const quotes = {};
+
+let etag = crypto.createHash('sha1').update(JSON.stringify(quotes));
+let digest = etag.digest('hex');
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const getCss = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+  response.write(css);
+  response.end();
+};
+
+// with a JSON body (GET)
+const respond = (request, response, status, content) => {
+  const header = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+  response.writeHead(status, header);
+  response.write(JSON.stringify(content));
+  response.end();
+};
+
+// no JSON body (HEAD)
+const respondJSONMeta = (request, response, status) => {
+  const header = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+  response.writeHead(status, header);
+  response.end();
+};
+
+// GET
+const getQuote = (request, response) => {
+  /* if (!quotes[name]) {
+    const responseJSON = {
+      text: 'No quote by this person',
+    };
+  } else {
+    const responseJSON = {
+      text: quotes[body.name].quote,
+    };
+  }*/
+
+  const responseJSON = {
+    quotes,
+  };
+
+    // check the client's if-none-match header to see the
+    // number the client is returning from etag
+    // If the version number (originally set by the server in
+    // etag) is the same as our current one, then send a 304
+    // 304 cannot have a body in it
+  if (request.headers['if-none-match'] === digest) {
+        // return 304 response without message
+        // 304 is not modified and cannot have a body field
+        // 304 will tell the browser to pull from cache instead
+    return respondJSONMeta(request, response, 304);
+  }
+
+    // return 200 with message
+  return respond(request, response, 200, responseJSON);
+};
+
+
+// HEAD
+const getQuoteMeta = (request, response) => {
+  if (request.headers['if-none-match'] === digest) {
+    return respondJSONMeta(request, response, 304);
+  }
+
+  return respondJSONMeta(request, response, 200);
+};
+
+
+const addQuote = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and quote are both required.',
+  };
+
+  if (!body.name || !body.quote) {
+    responseJSON.id = 'missingParams';
+    return respond(request, response, 400, responseJSON);
+  }
+
+    // the default is 201
+  let responseCode = 201;
+
+    //
+  if (quotes[body.name]) {
+    responseCode = 204;
+  } else {
+    quotes[body.name] = {};
+  }
+
+    // throw the new quotes into the object
+  quotes[body.name].name = body.name;
+  quotes[body.name].quote = body.quote;
+
+    // creating a new hash object
+  etag = crypto.createHash('sha1').update(JSON.stringify(quotes));
+    // recalculating the hash digest for etag
+  digest = etag.digest('hex');
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    responseJSON.name = quotes[body.name].name;
+    responseJSON.quote = quotes[body.name].quote;
+    return respond(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+// function for 404 not found requests with message
+const notFound = (request, response) => {
+  // create error message for response
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return a 404 with an error message
+  respond(request, response, 404, responseJSON);
+};
+
+// function for 404 not found without message
+const notFoundMeta = (request, response) => {
+  // return a 404 without an error message
+  respondJSONMeta(request, response, 404);
+};
+
+// exports to set functions to public
+module.exports = {
+  getIndex,
+  getCss,
+  getQuote,
+  getQuoteMeta,
+  addQuote,
+  notFound,
+  notFoundMeta,
+};
+
---
+++ src/server.js
@@ -0,0 +1,98 @@
+// Code taken from Aidan Kaufman Http-Api-Assignment-2
+const http = require('http');
+const url = require('url');
+const handler = require('./responses.js');
+const query = require('querystring');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  const parsedUrl = url.parse(request.url);
+
+  // check the request method (get, head, post, etc)
+  switch (request.method) {
+    case 'GET':
+      if (parsedUrl.pathname === '/') {
+        // if homepage, send index
+        handler.getIndex(request, response);
+      } else if (parsedUrl.pathname === '/style.css') {
+        // if stylesheet, send stylesheet
+        handler.getCss(request, response);
+      } else if (parsedUrl.pathname === '/getQuote') {
+        // if get Quote, send Quote object back
+        /* const res = response;
+
+        const body = [];
+
+        request.on('error', () => {
+          res.statusCode = 400;
+          res.end();
+        });
+
+        request.on('data', (chunk) => {
+          body.push(chunk);
+        });
+
+        request.on('end', () => {
+          const bodyString = Buffer.concat(body).toString();
+
+          const bodyParams = query.parse(bodyString);
+
+          handler.getQuote(request, res, bodyParams);
+        });*/
+        handler.getQuote(request, response);
+//      }
+//        else if (parsedUrl.pathname === '/updateUser') {
+//        // if update user, change our user object
+//        handler.updateUser(request, response);
+      } else {
+        // if not found, send 404 message
+        handler.notFound(request, response);
+      }
+      break;
+    case 'HEAD':
+      if (parsedUrl.pathname === '/getQuote') {
+        // if get Quote, send meta data back with etag
+        handler.getQuoteMeta(request, response);
+      } else {
+        // if not found send 404 without body
+        handler.notFoundMeta(request, response);
+      }
+      break;
+    case 'POST':
+      if (parsedUrl.pathname === '/addQuote') {
+        const res = response;
+
+        const body = [];
+
+        request.on('error', () => {
+          res.statusCode = 400;
+          res.end();
+        });
+
+        request.on('data', (chunk) => {
+          body.push(chunk);
+        });
+
+        request.on('end', () => {
+          const bodyString = Buffer.concat(body).toString();
+
+          const bodyParams = query.parse(bodyString);
+
+          handler.addQuote(request, res, bodyParams);
+        });
+      }
+      break;
+    default:
+      // send 404 in any other case
+      handler.notFound(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
+
